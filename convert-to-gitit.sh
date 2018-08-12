346d4ef836dc0aefea49cc23d44a9800487a90dd
Sam Toyer <sam@qxcv.net>
2018-08-12 11:53:21 -0700

    gitit converter

--- convert-to-gitit.sh
+++ convert-to-gitit.sh
@@ -5,4 +5,24 @@
 
 set -e
 
-PANDOC_CMD="pandoc --from html+tex_math_dollars --to markdown+tex_math_dollars+hard_line_breaks"
+OUTPUT_DIR="./hatta/"
+FILE_PREFIX="hatta-files"
+
+if [ "$#" != 1 ]; then
+    echo "USAGE: $0 <path to Hatta config>"
+    exit 1
+fi
+hatta_config="$1"
+
+echo "Reading from $hatta_config. Will write to $OUTPUT_DIR."
+./convert.py --file-prefix "$FILE_PREFIX" "$hatta_config" "$OUTPUT_DIR"
+find "$OUTPUT_DIR" -type f -name '*.html' -not -path "$OUTPUT_PREFIX/$FILE_PREFIX" \
+| while read html_path; do
+    page_path="$(echo "$html_path" | sed -e 's/\.html$/.page/')"
+    echo "$html_path -> $page_path"
+    pandoc \
+        --from html+tex_math_dollars \
+        --to markdown+tex_math_dollars+hard_line_breaks \
+        < "$html_path" > "$page_path"
+    rm "$html_path"
+done
